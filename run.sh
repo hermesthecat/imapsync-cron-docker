4d65b9f06859972e31725bc2e84dc47c2c3be1bb
afalter <48165619+afalter@users.noreply.github.com>
2020-02-02 23:53:12 +0100

    Added real command

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "System path is $PATH at $timestamp"
+
+imapsync --host1 $HOST1 --user1 $USER2 --passfile1 /config/pass1 --host2 $HOST2 --user2 $USER2 --passfile2 /config/pass2 --delete2
