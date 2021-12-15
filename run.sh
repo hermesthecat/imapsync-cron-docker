0dcb091911a0ba7dcfd6e88a8179660324a65285
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2021-12-15 10:38:21 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-imapsync --host1 $HOST1 --user1 $USER1 --passfile1 /config/pass1 --host2 $HOST2 --user2 $USER2 --passfile2 /config/pass2 --noreleasecheck --useheader "Message-Id" --addheader --delete2duplicates --regextrans2 "s{Sent|Sent Messages|Gesendet}{Sent Items}" --regextrans2 "s{Trash}{Deleted Items}" --regextrans2 "s{Spam}{Junk Email}"  
+imapsync --host1 $HOST1 --user1 $USER1 --passfile1 /config/pass1 --host2 $HOST2 --user2 $USER2 --passfile2 /config/pass2 --noreleasecheck --useheader "Message-Id" --addheader --delete2duplicates --automap
